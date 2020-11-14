2a3d011859124e94bddbc639fb4490a7dff8c6e7
akhrtj <akihiro.02.tj@gmail.com>
2020-11-14 17:13:29 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+import styles from './App.module.scss';
+
+export const App: FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Demo</h1>
+    </div>
+  );
+};
