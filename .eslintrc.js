2838d6739c3aa5a55404e8310f012833fd85884f
Akihiro Tojo <akihiro.02.tj@gmail.com>
2020-12-07 23:54:19 +0900

    add eslint-plugin-jsx-a11y

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
     'prettier/react',
