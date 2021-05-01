0ed18ed962d920b3eccbecb323d67acca9e9fdab
Akihiro Tojo <akihiro.021109@gmail.com>
2021-05-01 17:09:45 +0900

    add tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
 import React from 'react';
+
 import ReactDOM from 'react-dom';
 
+import './index.scss';
 import { App } from './containers/App';
-
-import './styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
